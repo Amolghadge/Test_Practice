28685453325270e5ac14fca96e86f0d0d46267c5
Amol Ghadge <64397760+Amolghadge@users.noreply.github.com>
2025-06-07 16:15:40 +0530

    Update main.tf

--- Test/main.tf
+++ Test/main.tf
@@ -1,5 +1,6 @@
 # Configure the Google Cloud Provider
 #To test the conflict#
+#Updated comment in main#
 provider "google" {
   project = "your-gcp-project-id" # REPLACE with your GCP Project ID
   region  = "us-central1"          # Change to your desired GCP region
